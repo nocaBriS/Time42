b934d9d182f83b43e03ec094944e0688f55da200
lisahelen <obermayr.lisa@sus.htl-grieskirchen.at>
2021-01-28 08:55:05 +0100

    set name on home page

--- app/src/main/java/com/example/time42/Home/HomeFragment.java
+++ app/src/main/java/com/example/time42/Home/HomeFragment.java
@@ -2,6 +2,8 @@ package com.example.time42.Home;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,12 +19,18 @@ import com.example.time42.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private TextView nameView;
+    SharedPreferences sharedPreferences;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        sharedPreferences = getActivity().getSharedPreferences("logPref", Context.MODE_PRIVATE);
+        nameView = root.findViewById(R.id.nameView);
+        nameView.setText(sharedPreferences.getString("name", "Name"));
 
         final TextView textView = root.findViewById(R.id.textView7);
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);

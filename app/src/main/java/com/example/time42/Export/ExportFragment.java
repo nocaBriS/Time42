d3511ebcecb92f618d93db8c99b2b340970186f4
lisahelen <obermayr.lisa@sus.htl-grieskirchen.at>
2021-03-11 10:22:24 +0100

    finished enter date

--- app/src/main/java/com/example/time42/Export/ExportFragment.java
+++ app/src/main/java/com/example/time42/Export/ExportFragment.java
@@ -2,6 +2,7 @@ package com.example.time42.Export;
 
 import android.os.Bundle;
 
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,8 +17,7 @@ import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClic
 
 public class ExportFragment extends Fragment {
 
-    EditText etVon;
-    EditText etBis;
+    EditText etDateRange;
     MaterialDatePicker mdp;
 
     public ExportFragment() {
@@ -28,37 +28,30 @@ public class ExportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_export, container, false);
-        etVon = root.findViewById(R.id.etVon);
-        etBis = root.findViewById(R.id.etBis);
+        etDateRange = root.findViewById(R.id.etDatePicker);
 
 
-        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
-        buildDatePicker(materialDateBuilder);
+        MaterialDatePicker.Builder<Pair<Long, Long>> materialDateBuilder = MaterialDatePicker.Builder.dateRangePicker();
+        materialDateBuilder.setTitleText("Wähle Zeitraum");
+        mdp = materialDateBuilder.build();
 
-        etVon.setOnClickListener(v -> openDatePicker(v, true));
-        etBis.setOnClickListener(v -> openDatePicker(v, false));
+        etDateRange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mdp.show(getFragmentManager(), "MATERIAL_DATE_PICKER");
+
+            }
+        });
+
+        mdp.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                etDateRange.setText(mdp.getHeaderText());
+            }
+        });
+
 
         return root;
-    }
-
-    private void openDatePicker(View v, boolean isVon) {
-        mdp.show(getFragmentManager(), "MATERIAL_DATE_PICKER");
-            mdp.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
-                @Override
-                public void onPositiveButtonClick(Object selection) {
-                    if(isVon) {
-                        etVon.setText(mdp.getHeaderText());
-                    } else {
-                        etBis.setText(mdp.getHeaderText());
-                    }
-                }
-            });
-
-    }
-
-    private void buildDatePicker(MaterialDatePicker.Builder builder) {
-        builder.setTitleText("Select Date");
-        mdp = builder.build();
     }
 
 }

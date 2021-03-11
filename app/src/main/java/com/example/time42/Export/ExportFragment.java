85deaad25d04452e716fc1be1edd266c3fcaee64
lisahelen <obermayr.lisa@sus.htl-grieskirchen.at>
2021-03-11 09:26:07 +0100

    add exportfragment class and layout xml

---
+++ app/src/main/java/com/example/time42/Export/ExportFragment.java
@@ -0,0 +1,26 @@
+package com.example.time42.Export;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.time42.R;
+
+
+public class ExportFragment extends Fragment {
+
+    public ExportFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_export, container, false);
+    }
+}
